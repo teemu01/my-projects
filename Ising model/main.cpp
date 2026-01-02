d8d41b0baaabaad1b9587a9b142192c3a591f187
teemu01 <167751062+teemu01@users.noreply.github.com>
2026-01-02 18:17:32 +0200

    Added susceptibility and specific heat calculations and ROOT plotting for the observables

--- Ising model/main.cpp
+++ Ising model/main.cpp
@@ -3,6 +3,9 @@
 #include <cstdlib>
 #include <cmath>
 #include <ctime>
+#include <TCanvas.h>
+#include <TGraph.h>
+#include <TAxis.h>
 
 using namespace std;
 
@@ -15,6 +18,13 @@ void monte_carlo_move(vector<vector<int>> &, int, double, double, double);
 double system_hamiltonian(const vector<vector<int>> &, int, double, double);
 double system_magnetization(const vector<vector<int>> &, int);
 void display_vector(const vector<double>);
+void compute_thermodynamical_quantities(int, double, double, int, int,
+                                const vector<vector<int>> &,
+                                const vector<double> &,
+                                vector<double> &, vector<double> &,
+                                vector<double> &, vector<double> &);
+
+
 
 
 // Prints 2D spin lattice, showing each site (+1 or -1) in matrix form
@@ -114,7 +124,7 @@ double system_magnetization(const vector<vector<int>> &spins, int N){
 }
 
 // prints all elements of vector
-void display_vector(const vector<double> v){
+void display_vector(const vector<double> &v){
     for (auto i : v){
         cout << i << " ";
     }
@@ -122,57 +132,174 @@ void display_vector(const vector<double> v){
 }
 
 
-//This is a numerical simulation of Ising model using metropolis algorithm.
-int main(){
-    srand(time(nullptr));
+void compute_thermodynamical_quantities(int N, double J, double h, int thermalisation_number, int mean_number,
+                                vector<vector<int>> &spins,
+                                const vector<double> &temperature_values,
+                                vector<double> &energy_values,
+                                vector<double> &magnetization_values,
+                                vector<double> &susceptibility_values,
+                                vector<double> &specific_heat_values){
 
-    // lattice size is N x N
-    int N {20};
 
-    // Interaction term
-    double J {10};
-    // External field
-    double h {10};
 
-    vector<vector<int>> spins {};
-    vector<double> temperature_values {};
-    vector<double> magnetization_values {};
-    vector<double> energy_values {};
+    for (auto temperature: temperature_values){
+        double beta = 1/temperature;
 
-    initialize_lattice(spins, N);
-
-    double hamiltonian {};
-    double magnetization {};
-    double beta {};
-
-    for (int i = 0; i < 20; i++){
-        // Inverse temperature beta = 1/T
-        beta = 0.01 + i*0.002;
-
-        // for each beta performs 1000 monte carlo sweeps to thermalize the system
-        for (int j = 0; j < 1000; j++){
+        //Thermalisation of the Ising model
+        for (int j = 0; j < thermalisation_number; j++){
             monte_carlo_move(spins, N, J, beta, h);
-            hamiltonian = system_hamiltonian(spins, N, J, h);
-            magnetization = system_magnetization(spins, N);
         }
-        // Stores the magnetization and energy of the last sweep
-        temperature_values.push_back(beta);
+
+        double sum_magnetization {0.0};
+        double sum_magnetization_squared {0.0};
+        double sum_energy {0.0};
+        double sum_energy_squared {0.0};
+
+        //Loop for thermalised system to calculate the average energy and magnetization
+        for (int j = 0; j < mean_number; j++){
+            monte_carlo_move(spins, N, J, beta, h);
+
+            double E = system_hamiltonian(spins, N, J, h);
+            sum_energy += E;
+            sum_energy_squared += E*E;
+            double mag = system_magnetization(spins, N);
+            sum_magnetization += abs(mag);
+            sum_magnetization_squared += mag * mag;
+        }
+
+        double energy = sum_energy / mean_number;
+        double energy_squared = sum_energy_squared / mean_number;
+        double magnetization = sum_magnetization / mean_number;
+        double magnetization_squared = sum_magnetization_squared / mean_number;
+        double susceptibility = beta*N*N*(magnetization_squared - magnetization*magnetization);
+        double specific_heat = beta*beta*N*N*(energy_squared - energy*energy);
+
         magnetization_values.push_back(magnetization);
-        energy_values.push_back(hamiltonian);
+        energy_values.push_back(energy);
+        susceptibility_values.push_back(susceptibility);
+        specific_heat_values.push_back(specific_heat);
 
     }
+}
 
 
-    cout << "Temperature: " << endl;
-    display_vector(temperature_values);
-    cout << "\n Magnetization: " << endl;
-    display_vector(magnetization_values);
-    cout << "\n Energy: " << endl;
-    display_vector(energy_values);
-    cout << endl;
-    //display_vector(energy_values);
 
-    display(spins);
+
+//This is a numerical simulation of Ising model using metropolis algorithm.
+int main(){
+
+    // lattice size is N x N
+    int N {30};
+    // Interaction term
+    double J {1};
+    // External field
+    double h {0};
+    
+    double T_initial {1.5};
+    int thermalisation_number {5000};
+    int mean_number {3000};
+
+
+    //Initialize lattice with random spin values (+1 or -1) for each site
+    vector<vector<int>> spins {};
+    srand(time(nullptr));
+    initialize_lattice(spins, N);
+
+
+    //List of different thermodynamic quantities to derive
+    vector<double> magnetization_values {};
+    vector<double> susceptibility_values {};
+    vector<double> specific_heat_values {};
+    vector<double> energy_values {};
+
+
+    vector<double> temperature_values {};
+    for (int i = 0; i < 60; i++){
+        double temperature = T_initial + 0.02*i;
+        temperature_values.push_back(temperature);
+    }
+
+    compute_thermodynamical_quantities(N, J, h, thermalisation_number, mean_number,
+                                spins, temperature_values, energy_values,
+                                magnetization_values, susceptibility_values,
+                                specific_heat_values);
+
+
+
+    //#####################
+
+    TGraph *gr1 = new TGraph(temperature_values.size(),
+                 temperature_values.data(),
+                 magnetization_values.data());
+
+    TCanvas *c1 = new TCanvas("c1", "Graph Draw Options", 200, 10, 800, 600);
+
+
+    gr1->SetTitle("Magnetization vs Temperature;T;M");
+    gr1->SetMarkerStyle(20);
+    gr1->SetMarkerSize(1.2);
+    gr1->SetLineWidth(2);
+
+    //TF1 *fitFcn = new TF1("fitFcn",fitCurieFunction,1.5,2.3,2);
+
+    //gr1->Fit("fitFcn");
+
+    gr1-> Draw("AC*");
+
+    c1->SaveAs("magnetization_vs_temperature.png");
+
+    //#####################
+
+    TGraph *gr2 = new TGraph(temperature_values.size(),
+                 temperature_values.data(),
+                 energy_values.data());
+
+    TCanvas *c2 = new TCanvas("c2", "Graph Draw Options", 200, 10, 800, 600);
+
+    gr2->SetTitle("Energy vs Temperature;T;E");
+    gr2->SetMarkerStyle(20);
+    gr2->SetMarkerSize(1.2);
+    gr2->SetLineWidth(2);
+
+    gr2-> Draw("AC*");
+
+    c2->SaveAs("energy_vs_temperature.png");
+
+    //###############################
+
+    TGraph *gr3 = new TGraph(temperature_values.size(),
+                 temperature_values.data(),
+                 susceptibility_values.data());
+
+    TCanvas *c3 = new TCanvas("c3", "Graph Draw Options", 200, 10, 800, 600);
+
+    gr3->SetTitle("Susceptibility vs Temperature;T;X");
+    gr3->SetMarkerStyle(20);
+    gr3->SetMarkerSize(1.2);
+    gr3->SetLineWidth(2);
+
+    gr3-> Draw("AC*");
+
+    c3->SaveAs("susceptibility_vs_temperature.png");
+
+    //#######################################
+    TGraph *gr4 = new TGraph(temperature_values.size(),
+                 temperature_values.data(),
+                 specific_heat_values.data());
+
+    TCanvas *c4 = new TCanvas("c4", "Graph Draw Options", 200, 10, 800, 600);
+
+    gr4->SetTitle("Specific Heat vs Temperature;T;C");
+    gr4->SetMarkerStyle(20);
+    gr4->SetMarkerSize(1.2);
+    gr4->SetLineWidth(2);
+
+
+    gr4-> Draw("AC*");
+
+    c4->SaveAs("specific_heat_vs_temperature.png");
+
+
 
     return 0;
 }
